51a49f8c56a28ee8ce683a95ff70aa64b618d912
zahra <sabah.z3939@gmail.com>
2020-08-28 07:25:10 +0430

    small change for upload file

--- tarjome2/server/app.js
+++ tarjome2/server/app.js
@@ -13,7 +13,7 @@ mongoose.connect("mongodb://zarrr98:95243045@cluster0-shard-00-00-ediy8.mongodb.
 mongoose.Promise = global.Promise    
 app.use(cors());
 // app.use('/server/translationfiles',express.static("server/translationfiles"))  
-app.use(express.static(path.resolve(__dirname, 'build')));
+app.use(express.static(path.resolve(__dirname, '../build')));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/server', routes)
