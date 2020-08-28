76f180a679e8f23fbfdb0e10b4804892ea038278
zahra <sabah.z3939@gmail.com>
2020-08-28 08:36:37 +0430

    change something in nodemailer

--- tarjome2/server/email/send.js
+++ tarjome2/server/email/send.js
@@ -4,7 +4,7 @@ const transporter = nodemailer.createTransport({
   service:'Gmail',
   auth:{
     user : "translationsite1@gmail.com",
-    pass : "95243045"
+    pass : "strongPassWORD$$22"
   }
 })
 
